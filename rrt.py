95bd385129b9206323723029da193e67d05906e1
ABHISHEK SHARMA <asharm5@clemson.edu>
2022-11-10 22:51:21 -0500

    graph plotting

---
+++ rrt.py
@@ -0,0 +1,27 @@
+import pygame
+from rrtbase import RRTGraph
+from rrtbase import RRTMap
+
+def main():
+    dimensions = (600,1000)
+    start = (50,50)
+    goal = (510,510)
+    obsdim = 30
+    obsnum = 50
+
+    pygame.init()
+    map = RRTMap(start, goal, dimensions, obsdim, obsnum)
+    graph = RRTGraph(start, goal, dimensions, obsdim, obsnum)
+    
+    obstacles = graph.makeobs()
+
+    map = map.drawMap(obstacles)
+
+    pygame.display.update()
+    pygame.event.clear()
+    pygame.event.wait(0)
+
+
+if __name__== '__main__':
+    main()
+    
--- rrtbase.py
+++ rrtbase.py
@@ -1,3 +1,153 @@
 import pygame
-a = 5
-print(a)
+import random
+import math
+
+class RRTMap:
+    def __init__(self, start, goal, MapDimensions, obsdim, obsnum):
+        self.start = start
+        self.goal = goal
+        self.MapDimensions = MapDimensions
+        self.Maph, self.Mapw = MapDimensions
+
+        # window settings
+        self.MapWindowName = 'RRT path planning'
+        pygame.display.set_caption(self.MapWindowName)
+        self.map = pygame.display.set_mode((self.Mapw, self.Maph))
+        self.map.fill((255,255,255))
+        self.nodeRad = 0
+        self.nodeThickness = 0
+        self.edgeThickness = 1
+
+        self.obstacles = []
+        self.obsdim = obsdim
+        self.obsnum = obsnum
+
+        #colors
+        self.grey = (70,70,70)
+        self.Blue = (0,0,255)
+        self.Green = (0,255,0)
+        self.Red = (255,0,0)
+        self.white = (255,255,255)
+
+
+
+
+    def drawMap(self,obstacles):
+        pygame.draw.circle(self.map,self.Green,self.start,self.nodeRad+5,0)
+        pygame.draw.circle(self.map,self.Red, self.goal, self.nodeRad+20,1)
+        self.drawObs(obstacles)
+        
+
+    def drawPath(self):
+        pass
+
+    def drawObs(self,obstacles):
+        obstacleList = obstacles.copy()
+        while(len(obstacleList)>0):
+            obstacle = obstacleList.pop(0)
+            pygame.draw.rect(self.map, self.grey, obstacle)
+        
+
+class RRTGraph:
+    def __init__(self, start, goal, MapDimensions, obsdim, obsnum):
+        (x,y) = start
+        self.start = start
+        self.goal = goal
+        self.goalFlag = False   # set true when tree reaches goal
+        self.maph, self.mapw = MapDimensions
+        self.x = []
+        self.y = []
+        self.parent = []
+
+        # initialize the tree
+        self.x.append(x)
+        self.y.append(y)
+        self.parent.append(0)
+
+        # obstacles
+        self.obstacles = []
+        self.obsDim = obsdim
+        self.obsNum = obsnum
+
+        # path
+        self.goalstate = None
+        self.path = []
+
+
+
+    def makeRandomRect(self):
+        uppercornerx = int(random.uniform(0,self.mapw - self.obsDim))
+        uppercornery = int(random.uniform(0,self.maph - self.obsDim))
+
+        return (uppercornerx,uppercornery)
+
+
+    def makeobs(self):
+        obs = []
+
+        for i in range(self.obsNum):
+            rectang = None
+            startgoalcol = True
+            while startgoalcol:
+                upper = self.makeRandomRect()
+                rectang = pygame.Rect(upper, (self.obsDim,self.obsDim))
+                if rectang.collidepoint(self.start) or rectang.collidepoint(self.goal):
+                    startgoalcol = True
+                else:
+                    startgoalcol = False
+
+            obs.append(rectang)
+        self.obstacles = obs.copy()
+        return obs
+
+
+    def add_node(self):
+        pass
+
+    def remove_node(self):
+        pass
+
+    def add_edge(self):
+        pass
+
+    def remove_edge(self):
+        pass
+
+    def number_of_nodes(self):
+        pass
+
+    def distance(self):
+        pass
+
+    def nearest(self):
+        pass
+
+    def isFree(self):
+        pass
+
+    def crossObstacle(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def step(self):
+        pass
+
+    def path_to_goal(self):
+        pass
+
+    def getPathCoords(self):
+        pass
+
+    def bias(self):
+        pass
+
+    def expand(self):
+        pass
+
+    def cost(self):
+        pass
+
+
+
