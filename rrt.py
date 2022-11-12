96c7ddd34eb788469b8e0561f4b044cd1939ad5b
ABHISHEK SHARMA <asharm5@clemson.edu>
2022-11-11 23:24:25 -0500

    functional

--- rrt.py
+++ rrt.py
@@ -6,8 +6,9 @@ def main():
     dimensions = (600,1000)
     start = (50,50)
     goal = (510,510)
-    obsdim = 30
+    obsdim = 50
     obsnum = 50
+    iteration = 0
 
     pygame.init()
     map = RRTMap(start, goal, dimensions, obsdim, obsnum)
@@ -15,13 +16,34 @@ def main():
     
     obstacles = graph.makeobs()
 
-    map = map.drawMap(obstacles)
+    map.drawMap(obstacles)
 
+    while(not graph.path_to_goal()):
+        if iteration%20 == 0:
+            X,Y,Parent = graph.bias(goal)
+            pygame.draw.circle(map.map, map.grey, (X[-1],Y[-1]),map.nodeRad+2,0)
+            pygame.draw.line(map.map,map.Blue, (X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),map.edgeThickness)
+        else:
+            X,Y,Parent = graph.expand()
+            pygame.draw.circle(map.map,map.grey, (X[-1],Y[-1]),map.nodeRad+2,0)
+            pygame.draw.line(map.map,map.Blue, (X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),map.edgeThickness)
+            
+        if iteration%5 == 0:
+            pygame.display.update()
+        iteration+=1
+    map.drawpath(graph.getPathCoords())
     pygame.display.update()
     pygame.event.clear()
     pygame.event.wait(0)
 
 
+
+
 if __name__== '__main__':
-    main()
-    
+    result = False
+    while not result:
+        try:
+            main()
+            result = True
+        except:
+            result = False
--- rrtbase.py
+++ rrtbase.py
@@ -14,7 +14,7 @@ class RRTMap:
         pygame.display.set_caption(self.MapWindowName)
         self.map = pygame.display.set_mode((self.Mapw, self.Maph))
         self.map.fill((255,255,255))
-        self.nodeRad = 0
+        self.nodeRad = 2
         self.nodeThickness = 0
         self.edgeThickness = 1
 
@@ -38,8 +38,14 @@ class RRTMap:
         self.drawObs(obstacles)
         
 
-    def drawPath(self):
-        pass
+    def drawpath(self,path):
+        for node in path:
+            pygame.draw.circle(self.map, self.Green, node, self.nodeRad+5,1)
+            #pygame.time.delay(10)
+            
+        for i in range(1,len(path)):
+            pygame.draw.line(self.map,self.Green,path[i],path[i-1],self.edgeThickness+2)
+
 
     def drawObs(self,obstacles):
         obstacleList = obstacles.copy()
@@ -101,50 +107,142 @@ class RRTGraph:
         return obs
 
 
-    def add_node(self):
-        pass
+    def add_node(self,n,x,y):
+        self.x.insert(n,x)
+        self.y.append(y)
+        
 
-    def remove_node(self):
-        pass
+    def remove_node(self,n):
+        self.x.pop(n)
+        self.y.pop(n)
 
-    def add_edge(self):
-        pass
+    def add_edge(self,parent,child):
+        self.parent.insert(child,parent)
 
-    def remove_edge(self):
-        pass
+    def remove_edge(self,n):
+        self.parent.pop(n)
 
     def number_of_nodes(self):
-        pass
+        return len(self.x)
 
-    def distance(self):
-        pass
+    def distance(self,n1,n2):
+        (x1,y1) = (self.x[n1], self.y[n1])
+        (x2,y2) = (self.x[n2], self.y[n2])
+        px = (float(x1) - float(x2))**2
+        py = (float(y1) - float(y2))**2
+        return (px+py)**(0.5)
 
-    def nearest(self):
-        pass
+    def sample_envir(self):
+        x = int(random.uniform(0, self.mapw))
+        y = int(random.uniform(0, self.maph))
+        return x,y
+
+    def nearest(self, n):
+        dmin = self.distance(0, n)
+        nnear = 0
+        for i in range(0, n):
+            if self.distance(i, n) < dmin:
+                dmin = self.distance(i, n)
+                nnear = i
+        return nnear
+    
 
     def isFree(self):
-        pass
+        n = self.number_of_nodes() - 1
+        (x,y) = (self.x[n], self.y[n])
+        obs = self.obstacles.copy()
+        while len(obs):
+            rectang = obs.pop(0)
+            if rectang.collidepoint(x,y):
+                self.remove_node(n)
+                return False
+        return True
 
-    def crossObstacle(self):
-        pass
 
-    def connect(self):
-        pass
+    def crossObstacle(self,x1,x2,y1,y2):
+        obs = self.obstacles.copy()
+        while len(obs)>0:
+            rectang = obs.pop(0)
+            for i in range(101):
+                u = i/100
+                x = x1*u + x2*(1-u)
+                y = y1*u + y2*(1-u)
+                if rectang.collidepoint(x,y):
+                    return True
+        
+        return False
 
-    def step(self):
-        pass
+    def connect(self,n1,n2):
+        (x1,y1) = (self.x[n1], self.y[n1])
+        (x2,y2) = (self.x[n2], self.y[n2])
+        if self.crossObstacle(x1,x2,y1,y2):
+            self.remove_node(n2)
+            return False
+        else:
+            self.add_edge(n1,n2)
+            return True
+
+
+    def step(self,nnear,nrand,dmax=35):
+        d = self.distance(nnear,nrand)
+        if d>dmax:
+            u = dmax/d
+            (xnear,ynear) = (self.x[nnear], self.y[nnear])
+            (xrand,yrand) = (self.x[nrand], self.y[nrand])
+            (px,py) = (xrand-xnear,yrand-ynear)
+            theta = math.atan2(py,px)
+            (x,y) = (int(xnear+dmax*math.cos(theta)),int(ynear+dmax*math.sin(theta)))
+            self.remove_node(nrand)
+            if abs(x-self.goal[0])<dmax and abs(y-self.goal[1])<dmax:
+                self.add_node(nrand,self.goal[0],self.goal[1])
+                self.goalstate = nrand
+                self.goalFlag = True
+
+            else:
+                self.add_node(nrand,x,y)
+        
 
     def path_to_goal(self):
-        pass
+        if self.goalFlag:
+            self.path = []
+            self.path.append(self.goalstate)
+            newpos=self.parent[self.goalstate]
+            while newpos!=0:
+                self.path.append(newpos)
+                newpos=self.parent[newpos]
+            self.path.append(0)
+        return self.goalFlag
 
     def getPathCoords(self):
-        pass
+        pathCoords =[]
+        for node in self.path:
+            x,y=(self.x[node],self.y[node])
+            pathCoords.append((x,y))
+        return pathCoords
 
-    def bias(self):
-        pass
+
+
+
+
+    def bias(self, ngoal):
+        n = self.number_of_nodes()
+        self.add_node(n, ngoal[0], ngoal[1])
+        nnear = self.nearest(n)
+        self.step(nnear, n)
+        self.connect(nnear, n)
+        return self.x, self.y, self.parent
+
+        
 
     def expand(self):
-        pass
+        n = self.number_of_nodes()
+        x, y = self.sample_envir()
+        self.add_node(n, x, y)
+        if self.isFree():
+            xnearest = self.nearest(n)
+            self.step(xnearest, n)
+            self.connect(xnearest, n)
+        return self.x, self.y, self.parent
 
     def cost(self):
         pass
